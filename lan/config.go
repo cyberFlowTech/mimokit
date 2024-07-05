3374911b4d40c460f9dba10dc886ace197148c36
leoncyberflow <leoncyberflow@cyberflow.cc>
2024-07-05 19:25:50 +0800

    add thai

--- lan/config.go
+++ lan/config.go
@@ -15,4 +15,5 @@ var SupportLan = map[string]language.Tag{
 	"mn_MN": language.Mongolian,          // 蒙古语
 	"pt_PT": language.Portuguese,         // 葡萄牙语
 	"es_ES": language.Spanish,            // 西班牙语
+	"th_TH": language.Thai,               // 西班牙语
 }
